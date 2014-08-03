ddef92df62cce925fb739add2f53eaec08543dbb
Geroge <Spartan322@live.com>
2014-08-03 19:26:21 -0400

    Update SEPhysics.cpp

--- physics/src/SEPhysics.cpp
+++ physics/src/SEPhysics.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
 #include "SEPhysics.h"
 
 SEPhysics::SEPhysics()
 {
+}
+
+SEPhysics SEPhysics::getInstance()
+{
+	if(instance == NULL)
+		instance = new SEPhysics();
+	return instance;
+}
+
+void SEPhysics::setGravity(SEVector gravity)
+{
+	this.gravity = gravity;
 }
