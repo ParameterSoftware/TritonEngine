321777f308c2088044cc2def96fad0dd4e23015a
Geroge <Spartan322@live.com>
2014-08-03 18:39:54 -0400

    Update SEVector.h

--- files/src/SEVector.h
+++ files/src/SEVector.h
@@ -1,5 +1,29 @@
 #ifndef __SEVECTOR__
 #define __SEVECTOR__
 
+template< class T>
+
+class SEVector
+{
+public:
+	T x, y, z;
+	SEVector(T xyz);
+	SEVector(T xy, T z);
+	SEVector(T x, T y, T z);
+	T getX();
+	T getY();
+	T getZ();
+	bool checkFor(T check);
+	SEVector operator+(SEVector other);
+	SEVector operator-(SEVector other);
+	SSEVector operator*(SEVector other);
+	SEVector operator/(SEVector other);
+	SEVector operator=(SEVector other);
+	SEVector operator+(T other);
+	SEVector operator-(T other);
+	SEVector operator*(T other);
+	SEVector operator/(T other);
+	SEVector operator=(T other);
+}
 
 #endif //define(__SEVECTOR__)
