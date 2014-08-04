c76a83a0442cdfc7464e87b306096994139b68c1
Geroge <Spartan322@live.com>
2014-08-04 01:14:02 -0400

    Create SENSocket.h

---
+++ network/src/SENSocket.h
@@ -0,0 +1,37 @@
+#include "SEN.h"
+
+#ifndef __SENSOCKET__
+#define __SENSOCKET__
+
+class SENAddress
+{
+public:
+	Address();
+	Address( unsigned char a, unsigned char b, unsigned char c, unsigned char d, unsigned short port );
+	Address( unsigned int address, unsigned short port );
+	unsigned int GetAddress() const;
+	unsigned char GetA() const;
+	unsigned char GetB() const;
+	unsigned char GetC() const;
+	unsigned char GetD() const;
+	unsigned short GetPort() const;
+private:
+	unsigned int address;
+	unsigned short port;
+};
+
+class SENSocket
+{
+public:
+	Socket();
+	~Socket();
+	bool Open( unsigned short port );
+	void Close();
+	bool IsOpen() const;
+	bool Send( const Address & destination, const void * data, int size );
+	int Receive( Address & sender, void * data, int size );
+private:
+	int handle;
+};
+
+#endif /* define(__SENSOCKET__) */
