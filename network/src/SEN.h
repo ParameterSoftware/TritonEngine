bb1f784f29a1351c2ed242222038845504a65276
Geroge <Spartan322@live.com>
2014-08-04 00:57:33 -0400

    Create SEN.h

---
+++ network/src/SEN.h
@@ -0,0 +1,34 @@
+// platform detection
+
+#define PLATFORM_WINDOWS  1
+#define PLATFORM_MAC      2
+#define PLATFORM_UNIX     3
+
+#if defined(_WIN32)
+#define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM PLATFORM_MAC
+#else
+#define PLATFORM PLATFORM_UNIX
+#endif
+
+#if PLATFORM == PLATFORM_WINDOWS
+#include <winsock2.h>
+#pragma comment( lib, "wsock32.lib" )
+#elif PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#endif
+
+class SocketManager
+{
+private:
+	static SocketManager instance;
+	SocketManager();
+public:
+	static SocketManager getInstance();
+	~SocketManager();
+	bool InitializeSockets();
+	void ShutdownSockets()
+};
