ae6d89319f3ec7c47f45dffc8c401630b237e25d
Geroge <Spartan322@live.com>
2014-08-07 17:53:42 -0400

    Create SEU.cpp

---
+++ utilities/src/SEU.cpp
@@ -0,0 +1,14 @@
+#include "SEU.h"
+
+int seuConvertToInt(char obj)
+{
+	int result = obj - '0';
+	return result;
+}
+
+char seuConvertToChar(int obj)
+{
+	int iresult = obj + '0';
+	char result = iresult;
+	return result;
+}
