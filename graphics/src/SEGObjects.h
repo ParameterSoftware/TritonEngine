1f038b1ca29ddc7a2e71345761a05b2c97339d43
Geroge <Spartan322@live.com>
2014-08-07 22:46:45 -0400

    Update SEGObjects.h

--- graphics/src/SEGObjects.h
+++ graphics/src/SEGObjects.h
@@ -1,9 +1,21 @@
+#include <iostream>
+#include "SEVector.h"
+
 #ifndef __SEGOBJECT__
 #define __SEGOBJECT__
 
 class SEGObject
 {
-
+public:
+	SEGObject(SEVector position);
+	virtual bool Start();
+	virtual void Update();
+	virtual void Stop();
+	void SetPosition(SEVector pos);
+	SEVector GetPosition();
+private:
+	SEVector position;
+	virtual void UpdateGraphics();
 };
 
 #endif /* define(__SEGOBJECT__) */
