05e095425b7bce801bed359abda40771a06cb224
Geroge <Spartan322@live.com>
2014-08-07 23:36:32 -0400

    Create SEGVbo.cpp

---
+++ graphics/src/SEGVbo.cpp
@@ -0,0 +1,34 @@
+#include "SEGVbo.h"
+
+SEGVbo::SEGVbo(SEGVertexHolder holder)
+{
+	glGenVertexArrays(1, &vaoID[0]);
+	glBindVertexArray(vaoID[0]);
+	glGenBuffers(1, vboID);
+	glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
+	float vertices[holder.triangles.size()*3];
+	for(int i = 0; i < holder.triangles.size(), i++)
+	{
+		vertices[i] = holder.triangles.at(i).a.vertex.x;
+		vertices[i+1] = holder.triangles.at(i).a.vertex.y;
+		vertices[i+2] = holder.triangles.at(i).a.vertex.z;
+		i+=3;
+	}
+	glBufferData(GL_ARRAY_BUFFER, holder.triangles.size()*3 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+	this->Render(holder);
+}
+
+void SEGVbo::Render(SEGVertexHolder holder)
+{
+	glBindVertexArray(vaoID[0]);
+	glDrawArrays(GL_TRIANGLES, 0, );
+	glBindVertexArray(0);
+}
+
+SEGVbo::~SEGVbo()
+{
+	
+}
