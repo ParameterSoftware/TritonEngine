6fe5d79cbbc441215e18cec58637e0f997010ea4
Geroge <Spartan322@live.com>
2014-08-09 02:11:31 -0400

    Update and rename SEPhysics.cpp to SEP.cpp

---
+++ physics/src/SEP.cpp
@@ -0,0 +1,26 @@
+#include "SEP.h"
+
+SEPGravity::SEPGravity(SEvector gravity)
+{
+	this->gravity = gravity;
+}
+
+SEPGravity::~SEPGravity() {}
+
+SEVector SEPGravity::GetGravity()
+{
+	return gravity;
+}
+
+SEPGravity3D::SEPGravity3D(SEVector gravity, SEVector pull_location)
+{
+	this->gravity = gravity;
+	this->pull_location = pull_location;
+}
+
+SEPGravity3D::~SEPGravity3D() {}
+
+SEVector SEPGravity3D::GetLocation()
+{
+	return pull_location;
+}
