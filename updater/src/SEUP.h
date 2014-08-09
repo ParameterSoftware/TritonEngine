92f1e9b55080ce5106042a56d655be08d6eed69c
Geroge <Spartan322@live.com>
2014-08-09 18:06:41 -0400

    Create SEUP.h

---
+++ updater/src/SEUP.h
@@ -0,0 +1,33 @@
+#ifndef __SEUP__
+#define __SEUP__
+
+#include "SENConnection.h"
+
+class SEUPGame
+{
+public:
+	SEUPGame(std::string game_name, std::string update_address);
+	~SEUPGame();
+	bool Update();
+	virtual bool OnFailedUpdate();
+private:
+	std::string game_name;
+	SENSocket update_socket;
+	SENAddress update_address;
+};
+
+class SEUPEngine
+{
+public:
+	SEUPEngine();
+	~SEUPEngine();
+	bool Update();
+	bool OnFailedUpdate();
+	void SetGameUpdater(SEUPGame updater);
+private:
+	std::string engine_name = "Spartan Engine";
+	SEUPGame game_updater;
+	SENSocket update_address;
+};
+
+#endif /* define(__SEUP__) */
