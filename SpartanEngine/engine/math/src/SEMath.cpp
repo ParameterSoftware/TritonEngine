5674d7acc12f281218a46cf3973012103ded48f5
George Albany <spartan322@live.com>
2014-09-01 14:57:34 -0400

    Trignometry Functions added

--- SpartanEngine/engine/math/src/SEMath.cpp
+++ SpartanEngine/engine/math/src/SEMath.cpp
@@ -1,12 +1,11 @@
 
 #include "SEMath.h"
 
-#define _USE_MATH_DEFINES
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>  
 
 //The most perfected method example (method 14) at http://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi 
+//According to the examples there, its better then std::sqrt(n)
 double inline __declspec (naked) __fastcall InternalSqrt(double n)
 {
 	_asm fld qword ptr [esp+4]
@@ -48,4 +47,43 @@ double semFractionTime(double start, double end, double current)
 double semSqrt(double square)
 {
 	return InternalSqrt(square);
+}
+
+//The C library already handles functions like sin, cos, tan, ect. perfectly enough
+double semSin(double x, bool convert)
+{
+	if(convert)
+		x = semToDegrees(x);
+	return std::sin(x);
+}
+
+double semCos(double x, bool convert)
+{
+	if(convert)
+		x = semToDegrees(x);
+	return std::cos(x);
+}
+
+double semTan(double x, bool convert)
+{
+	if(convert)
+		x = semToDegrees(x);
+	return std::tan(x);
+}
+
+double semAtan(double x, bool convert)
+{
+	if(convert)
+		x = semToDegrees(x);
+	return std::atan(x);
+}
+
+double semAtan2(double x, double y, bool convert)
+{
+	if(convert)
+	{
+		x = semToDegrees(x);
+		y = semToDegrees(y);
+	}
+	return std::atan2(x,y);
 }
--- SpartanEngine/engine/math/src/SEMath.h
+++ SpartanEngine/engine/math/src/SEMath.h
@@ -2,13 +2,16 @@
 #define __SEMATH__
 
 #include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#undef _USE_MATH_DEFINES
 #include "SEVector.h"
 
 /**
  *	Generates a random number
  *	
  *	Generates a random numbers using the standard c++ number generator
- *	with every part handled by the function
+ *	with srand handled by the function
  *
  *	@param lowerRange	The lowest possible number for the generator
  *	@param higherRange	The highest possible number for the generator
@@ -22,7 +25,7 @@ double semRandom(double lowerRange, double higherRange);
  *	
  *	Gets PI
  *
- *	@return \pi is returned
+ *	@return \pi
  */
 double semPI();
 
@@ -74,11 +77,59 @@ double semFractionTime(double start, double end, double current);
 /**
  *	Gets the square root of square
  *
+ *	Gets the square root of square but is much more accurate and
+ *	faster then the standard library
+ *
  *	@param square	The number to find the square root
  *
  *	@return The square root of square
  */
 double semSqrt(double square);
+
+/**
+ *
+ *	Gets the sin of x
+ *
+ *	Gets the sine of x in radians unless convert
+ *	is true
+ *	
+ *	@param x		The number to get the sin of
+ *	@param convert	Use to get a degree instead	Default: false
+ *	
+ *	@return	The sin of x (in radians unless convert = true)
+ *
+*/
+double semSin(double x, bool convert = false);
+
+/**
+ *
+ *	Gets the cos of x
+ *
+ *	Gets the cosine of x in radians unless convert
+ *	is true
+ *	
+ *	@param x		The number to get the cos of
+ *	@param convert	Use to get a degree instead	Default: false
+ *	
+ *	@return	The cos of x (in radians unless convert = true)
+ *
+*/
+double semCos(double x, bool convert = false);
+
+/**
+ *
+ *	Gets the tan of x
+ *
+ *	Gets the tangent of x in radians unless convert
+ *	is true
+ *	
+ *	@param x		The number to get the tan of
+ *	@param convert	Use to get a degree instead	Default: false
+ *	
+ *	@return	The tan of x (in radians unless convert = true)
+ *
+*/
+double semTan(double x, bool convert = false);
 
 
 #endif /* defined(__SEMATH__) */
